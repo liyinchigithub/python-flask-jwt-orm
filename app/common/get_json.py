99d708c47388e7fc440bd96b880dfd7af10183c2
李银池 <liyinchi@qq.com>
2022-05-11 21:53:22 +0800

    common

---
+++ app/common/get_json.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+# 读取json文件，转成python dict
+import os
+import sys
+import time
+import datetime
+import json
+from bunch import Bunch
+
+"""
+    @Time :2022/02/11
+    @Author :liyinchi
+    @File :jira_get.py
+    @Version：1.0
+    @Ddescription: 配置读取函数提供的功能和扩展，如下：配置文件是JSON格式；配置文件转换为配置类和配置字典
+"""
+class config():
+    def __init__(self,json_file):
+        self.json_file=json_file
+    # 读取json配置文件内容
+    def get_config_from_json(self):
+        """
+        将配置文件转换为配置类
+        :param json_file: json文件
+        :return: 配置信息
+        """
+        with open(self.json_file, 'r') as f:
+            config_dict = json.load(f)  # 配置字典
+        config = Bunch(config_dict)  # 将配置字典转换为类
+        return config
--- app/run.py
+++ app/run.py
@@ -8,24 +8,30 @@ from importlib.resources import path
 from flask import Flask, url_for, request, render_template, redirect, flash, session, make_response,Blueprint,jsonify
 from flask_wtf.file import FileField, FileRequired, FileAllowed  # 文件上传
 from flask import send_from_directory  # 发送静态文件
-from flask_cors import CORS  # 跨域访问
+from flasgger import Swagger # flask swagger
+from flasgger.utils import swag_from # flask swagger
+from flask_cors import *  # 跨域访问
 from werkzeug.utils import secure_filename
 from werkzeug.routing import BaseConverter # 正则表达式
 import os
 import uuid  # 生成随机字符串
 import json
-from api.login import * # [蓝图]模块化
-from api.upload import * # [蓝图]模块化
-from api.logout import * # [蓝图]模块化
-from api.register import * # [蓝图]模块化
+from api.login import * # 定义路由-登录 [蓝图]模块化
+from api.upload import * # 定义路由-上传文件
+from api.logout import * # 定义路由-退出登录
+from api.register import * # 定义路由-注册
 
+from common.token import *
 # 实例化Flask对象 app
 app = Flask(__name__, template_folder='./myProject/templates/',static_folder="./static/") # 访问静态文件夹下的文件 http://127.0.0.1:5876/static/文件名.jpg
+# 
+swagger = Swagger(app)
 
 # [允许跨域]
 CORS(app, supports_credentials=True)
+# CORS(app, resources=r'/*')
 
-# 注册路由（蓝图）
+# [注册路由（蓝图）]
 app.register_blueprint(login,url_prefix='/login')
 app.register_blueprint(upload,url_prefix='/upload')
 app.register_blueprint(logout,url_prefix='/logout')
@@ -37,10 +43,7 @@ print("上传文件存放路径为",os.path.dirname(os.path.abspath(__file__)))
 app.config['UPLOAD_FOLDER'] = 'upload/' # 注意 ：upload 前面不能加“/”
 # [文件上传文件大小限制
 app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024 # 10M
-# [jwt配置]
-app.config["JWT_SECRET"] = "JWT_SECRET_KEY"
-app.config["JWT_EXPIRY_HOURS"] = 60
-app.config["JWT_REFRESH_DAYS"] = 1
+
 
 # [动态路由参数]正则表达式
 class RegexConver(BaseConverter):
@@ -99,21 +102,23 @@ def user_regex(name):
     request.cookies.get('username')
     return {"msg": "success", "status": 200, "data": name}
 
+
 # [获取url ? 后面的参数]    request.args.to_dict()
 @app.route('/find', methods=['GET', 'POST'])
+@swag_from('./swagger_yaml/index.yaml')
 def find():
     get_data = request.args.to_dict()# 获取传入的params参数
     username = get_data.get('username')
     password = get_data.get('password')
-    return {"msg": "success", "status": 200, "data": {"username":username,"password":password}}
+    return {"msg": "success", "code": 200, "data": {"username":username,"password":password}}
 
 # 每个请求前执行
 @app.before_request
-def jwt_authentication():
+def before_request():
+    # print(request.url)
     # 判断是否为登录请求
-    print(request.url)
-    if ('login' in request.url):
-        print("登录接口不进行token验证，只进行token生成")
+    if ('login' in request.url or 'register' in request.url) and request.method == 'POST':
+        print("当前是登录、注册接口，不会进行token验证，只进行token校验")
         return
     else:
         """
@@ -142,9 +147,9 @@ def jwt_authentication():
                     g.refresh = payload.get('refresh')
             else:
                 # 校验失败  例如：Signature has expired 签名过期
-                return jsonify({"msg": "登录超时，请重新登录", "status": 401}) 
-        else:
-            return jsonify({"msg": "登录超时，请重新登录", "status": 401}) 
+                return jsonify({"msg": "登录超时，请重新登录", "code": 403}) 
+        # else:
+        #     return jsonify({"msg": "登录超时，请重新登录", "code": 403}) 
 
 
 
@@ -161,11 +166,10 @@ def after_request(resp):
     # 允许跨域访问
     resp.headers['Access-Control-Allow-Origin'] = '*'
     resp.headers['Access-Control-Allow-Methods'] = 'GET,POST,PUT,DELETE,OPTIONS'
-    resp.headers['Access-Control-Allow-Headers'] = 'x-requested-with,content-type'
-    # 生成token
-    token,refresh_token=generate_token("1000")# TODO 获取请求头userid
-    resp.headers['token'] = token
-    resp.headers['refresh_token'] = refresh_token
+    resp.headers['Access-Control-Allow-Headers'] = '*'#  允许跨域访问的请求头字段（星号表示全匹配）
+    
+
+    
     # 设置cookie，也可以把token放入cookie中
     # resp.set_cookie("token", token, max_age=3600)
     # resp.set_cookie("refresh_token", refresh_token, max_age=3600)
@@ -177,7 +181,7 @@ def after_request(resp):
 @app.errorhandler(404)
 def page_not_found(error):
     # return render_template('404.html'), 404
-    return {"msg": "fial", "status": 201, "data": error}
+    return {"msg": "fial", "code": 201, "data": error}
 
 # [启动服务器]
 class InvalidUsage(Exception):  # 继承父类Exception
@@ -207,88 +211,6 @@ def exception():
     raise InvalidUsage('No privilege to access the resource', status_code=403)
 
 
-# 生成token
-def generate_token(user_id, need_refresh_token=True):
-    """
-    生成token 和refresh_token
-    :param user_id: 用户id
-    :return: token2小时, refresh_token14天
-    """
-    pass
-    '生成时间信息'
-    current_time = datetime.utcnow()
-    '指定有效期  业务token -- 2小时,我们这里测试所以设置的秒数'
-    expire_time = current_time + \
-        timedelta(seconds=app.config['JWT_EXPIRY_HOURS'])
-
-    '生成业务token  refresh 标识是否是刷新token'
-    token = generate_jwt(
-        {'user_id': user_id, 'refresh': False}, expiry=expire_time)
-
-    '给刷新token设置一个默认值None'
-    refresh_token = None
-    '根据传入的参数判断是否需要生成刷新token'
-    '不需要生成的传入need_refresh_token=False,需要的传入True或不传使用默认值'
-    if need_refresh_token:
-        '指定有效期  刷新token -- 14天,我们这里测试所以设置的秒数'
-        refresh_expires = current_time + \
-            timedelta(seconds=app.config['JWT_REFRESH_DAYS'])
-        '生成刷新token'
-        refresh_token = generate_jwt(
-            {'user_id': user_id, 'refresh': True}, expiry=refresh_expires)
-    '返回这两个token'
-    return token, refresh_token
-
-def login_required(f):
-    '让装饰器装饰的函数属性不会变 -- name属性'
-    '第1种方法,使用functools模块的wraps装饰内部函数'
-
-    @functools.wraps(f)
-    def wrapper(*args, **kwargs):
-        if not g.user_id:
-            return {'message': 'User must be authorized.'}, 401
-        elif g.refresh:
-            return {'message': 'Do not use refresh token.'}, 403
-        else:
-            return f(*args, **kwargs)
-
-    '第2种方法,在返回内部函数之前,先修改wrapper的name属性'
-    # wrapper.__name__ = f.__name__
-    return wrapper
-
-def generate_jwt(payload, expiry, secret=None):
-    """
-    生成jwt
-    :param payload: dict 载荷
-    :param expiry: datetime 有效期
-    :param secret: 密钥
-    :return: jwt
-    """
-    _payload = {'exp': expiry}
-    _payload.update(payload)
-
-    if not secret:
-        secret = app.config['JWT_SECRET']
-
-    token = jwt.encode(_payload, secret, algorithm='HS256')
-    return token
-
-
-def verify_jwt(token, secret=None):
-    """
-    检验jwt
-    :param token: jwt
-    :param secret: 密钥
-    :return: dict: payload
-    """
-    if not secret:
-        secret = app.config['JWT_SECRET']
-    try:
-        payload = jwt.decode(token, secret, algorithms=['HS256'])
-    except jwt.PyJWTError as e:
-        print("jwt.PyJWTError：",e)
-        payload = None
-    return payload
 
 
 # [启动服务器]
